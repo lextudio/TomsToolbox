c937f1bd9288a3a2e8b31d406fc00b4a27329b0f
Lex Li <support@lextm.com>
2025-12-21 19:29:04 -0500

    Remove duplicate files

---
+++ src/TomsToolbox.Avalonia/Properties/AssemblyInfo.cs
@@ -0,0 +1,11 @@
+﻿using System.Reflection;
+using System.Resources;
+using System.Runtime.InteropServices;
+using System.Windows;
+using System.Windows.Markup;
+
+[assembly: AssemblyCulture("")]
+[assembly: CLSCompliant(true)]
+[assembly: ComVisible(false)]
+
+[assembly: NeutralResourcesLanguage("en-US", UltimateResourceFallbackLocation.MainAssembly)]
